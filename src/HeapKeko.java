2b0c931f09fd2da7b446fc8d2b852e5238d52420
Cosmos85 <esakesti@gmail.com>
2022-04-06 23:24:22 +0100

    assignment

---
+++ src/HeapKeko.java
@@ -0,0 +1,71 @@
+public class HeapKeko {
+
+    public void sort(int taulukko[])
+    {
+        int n = taulukko.length;
+
+        // Build heap (rearrange array)
+        for (int i = n / 2 - 1; i >= 0; i--)
+            heapify(taulukko, n, i);
+
+        // One by one extract an element from heap
+        for (int i = n - 1; i > 0; i--) {
+            // Move current root to end
+            int temp = taulukko[0];
+            taulukko[0] = taulukko[i];
+            taulukko[i] = temp;
+
+            // call max heapify on the reduced heap
+            heapify(taulukko, i, 0);
+        }
+    }
+
+    // To heapify a subtree rooted with node i which is
+    // an index in arr[]. n is size of heap
+    void heapify(int taulukko[], int n, int i)
+    {
+        int largest = i; // Initialize largest as root
+        int l = 2 * i + 1; // left = 2*i + 1
+        int r = 2 * i + 2; // right = 2*i + 2
+
+        // If left child is larger than root
+        if (l < n && taulukko[l] > taulukko[largest])
+            largest = l;
+
+        // If right child is larger than largest so far
+        if (r < n && taulukko[r] > taulukko[largest])
+            largest = r;
+
+        // If largest is not root
+        if (largest != i) {
+            int swap = taulukko[i];
+            taulukko[i] = taulukko[largest];
+            taulukko[largest] = swap;
+
+            // Recursively heapify the affected sub-tree
+            heapify(taulukko, n, largest);
+        }
+    }
+
+    /* A utility function to print array of size n */
+    static void printArray(int taulukko[])
+    {
+        int n = taulukko.length;
+        for (int i = 0; i < n; ++i)
+            System.out.print(taulukko[i] + " ");
+        System.out.println();
+    }
+
+    // Driver code
+    public static void main(String args[])
+    {
+        int taulukko[] = { 12, 11, 13, 5, 6, 7 };
+        int n = taulukko.length;
+
+        HeapKeko ob = new HeapKeko();
+        ob.sort(taulukko);
+
+        System.out.println("Sorted array is");
+        printArray(taulukko);
+    }
+}
--- src/PrioQu.java
+++ src/PrioQu.java
@@ -3,20 +3,20 @@ import java.util.PriorityQueue;
 public class PrioQu {
     public static void main(String[] args) {
         PriorityQueue<Integer> numbers = new PriorityQueue<>();
-        int vali,
+        int vali;
 
 
         numbers.add(4);
         numbers.add(7);
         numbers.add(3);
         for (int i = 0; i < numbers.size(); i++) {
-            if (a[i] > a[j])
-            {
+            if (a[i] > a[j]) {
                 vali = a[i];
                 a[i] = a[j];
                 a[j] = vali;
             }
-        System.out.println("PrioQu: " + numbers);
+            System.out.println("PrioQu: " + numbers);
 
+        }
     }
 }
