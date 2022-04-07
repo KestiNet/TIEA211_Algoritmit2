3ac9f9738667cf2ceef1aa3d29014ecdef4ffbac
Cosmos85 <esakesti@gmail.com>
2022-04-07 20:45:47 +0100

    Java HeapSort implementation to sort through an array of random integers

--- src/HeapKeko.java
+++ src/HeapKeko.java
@@ -6,11 +6,10 @@ import java.util.Random;
  */
 
 public class HeapKeko {
-
+    //Aletaan rakentamaan kekoa ja käydään taulukkoa läpi
     public void sort(int taulukko[])
     {
         int n = taulukko.length;
-        //Aletaan rakentamaan kekoa
         for (int i = n / 2 - 1; i >= 0; i--)
             kekoa(taulukko, n, i);
 
@@ -23,29 +22,27 @@ public class HeapKeko {
         }
     }
 
-    // To heapify a subtree rooted with node i which is
-    // an index in arr[]. n is size of heap
+
+    //Aletaan rakentamaan kekoa
+    //Kkäydään läpi vasen ja oikea lapsisolmu
     void kekoa(int taulukko[], int n, int i)
     {
-        int suurin = i; // Initialize largest as root
-        int j = 2 * i + 1; // left = 2*i + 1
-        int k = 2 * i + 2; // right = 2*i + 2
+        int suurin = i;
+        int j = 2 * i + 1;
+        int k = 2 * i + 2;
 
-        // If left child is larger than root
         if (j < n && taulukko[j] > taulukko[suurin])
             suurin = j;
 
-        // If right child is larger than largest so far
         if (k < n && taulukko[k] > taulukko[suurin])
             suurin = k;
 
-        // If largest is not root
+
         if (suurin != i) {
             int swappi = taulukko[i];
             taulukko[i] = taulukko[suurin];
             taulukko[suurin] = swappi;
 
-            // Recursively heapify the affected sub-tree
             kekoa(taulukko, n, suurin);
         }
     }
@@ -70,11 +67,10 @@ public class HeapKeko {
         int max = 317;
         Random satunnainen = new Random();
         int taulukko[] = new int[8];
-        //int n = taulukko.length;
         for(int i = 0; i < taulukko.length; ++i)
             taulukko[i] = satunnainen.nextInt(max + min) +min;
-        HeapKeko ob = new HeapKeko();
-        ob.sort(taulukko);
+        HeapKeko keko = new HeapKeko();
+        keko.sort(taulukko);
 
         System.out.println("Järjestetty taulukko: ");
         tulostaTaulukko(taulukko);
