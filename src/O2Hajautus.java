302eaed061b8a789c177d88914cb05fdb096c9cd
Cosmos85 <esakesti@gmail.com>
2022-04-21 22:38:31 +0100

    Need to start from beginning and redo the whole thing

--- src/O2Hajautus.java
+++ src/O2Hajautus.java
@@ -1,13 +1,70 @@
-import java.util.*;
+
+
+import java.util.LinkedList;
 
 
 public class O2Hajautus {
+    //attribuutit
 
+    private LinkedList<Alkio>[] hajautustaulu = new LinkedList[10000];
+    //Hajautustaulun alkioiden määrä
+    private int koko;
 
-    public static void main(String[] args){
+    //aksessorit
 
-
+    public int koko() {
+        return koko;
     }
 
-}
+    //rakentajat
 
+    public O2Hajautus() {
+        //lisätään jokaiseen taulukon alkioon linkitetty lista
+        for(int i=0; i<10000; i++) {
+            hajautustaulu[i] = new LinkedList<>();
+        }
+        koko = 0;
+    }
+
+    //metodit
+
+    /**
+     * Lisätään taulukkoon alkio, joka koostuu avain-arvo parista
+     * @param uusiAvain  taulukkoon talletettava avain
+     * @param uusiArvo  taulukkoon talletettava arvo
+     */
+    public void put(Integer uusiAvain, Integer uusiArvo) {
+        if (uusiAvain < 10000) {
+            Alkio lisättävä = new Alkio(uusiAvain, uusiArvo);
+            //lista pidetään lisäysjärjestyksessä
+            hajautustaulu[uusiAvain].addLast(lisättävä);
+            koko++;
+        }
+        else {
+            System.out.println("Luku " + uusiAvain + " on liian suuri");
+        }
+    }
+
+    /**
+     * Palautetaan lista alkioista, joiden avain on annettu
+     * @param indeksi avain jonka alkiot halutaan
+     * @return  lista alkioista jotka vastaavat haluttua avainta
+     */
+    public LinkedList<Alkio> get(Integer indeksi) {
+        return hajautustaulu[indeksi];
+    }
+
+    /**
+     * Poistetaan viimeksi lisätty alkio, joka kuuluu listaan annetussa indeksissä
+     * @param indeksi  Se indeksi, josta halutaan poistaa alkio
+     */
+    public void poista(int indeksi) {
+        if (hajautustaulu[indeksi].isEmpty()) {
+            System.out.println("Tälläistä alkiota ei löydy hajautustaulusta");
+        }
+        else {
+            hajautustaulu[indeksi].removeLast();
+            koko--;
+        }
+    }
+}
