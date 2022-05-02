b60528447fbd716b61ac2b5fa0672d5ce936944b
Esa Kesti <esakesti@gmail.com>
2022-05-02 17:52:57 +0100

    Update O3.java
    New class Solmu created and looping through array

--- src/O3.java
+++ src/O3.java
@@ -1,8 +1,19 @@
 
 public class O3 {
-
+	
+	public class Solmu{
+		
+		int linkki [] = {1,2,3,4,5};
+		String alkio [] =  {"kiuru", "lokki", "rastas", "sorsa", "varis"};
+	}
+	
+	int pvKoko = 5;
+	
+	Solmu[] pvJoukko = new Solmu[pvKoko];
+	for (int i = 0; i < pvJoukko.length; i++) {
+		pvJoukko[i] = new Solmu();
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
 
