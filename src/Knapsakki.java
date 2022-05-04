774a8f8fc1dbdfa7f63dee2722fc3b8d115399b0
Esa Kesti <esakesti@gmail.com>
2022-05-05 00:59:55 +0100

    knapsack problem solution
    knapsack assignment beginning

---
+++ src/Knapsakki.java
@@ -0,0 +1,36 @@
+import java.util.*;
+class Knapsakki {
+
+	
+	static int max(int a, int b)
+	{
+		return (a > b) ? a : b;
+	}
+
+	
+	static int sakki(int W, int wt[], int val[], int n){
+		int i, w;
+		int K[][] = new int[n + 1][W + 1];
+
+		for (i = 0; i <= n; i++) {
+			for (w = 0; w <= W; w++) {
+				if (i == 0 || w == 0) K[i][w] = 0;
+				else if (wt[i - 1] <= w) K[i][w] = Math.max(val[i - 1] + K[i - 1][w - wt[i - 1]], K[i - 1][w]);
+				else
+					K[i][w] = K[i - 1][w];
+			}
+		}
+
+		return K[n][W];
+	}
+
+	public static void main(String args[])
+	{
+		int val[] = new int[] { 17, 10, 110 };
+		int wt[] = new int[] { 13, 55, 41 };
+		
+		int W = 50;
+		int n = val.length;
+		System.out.println(sakki(W, wt, val, n));
+	}
+}
